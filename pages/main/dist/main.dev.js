dc202ac75c26d9727b3c09e6abbe69c39dcde3e2
Liza Prokopenko <elizaveta_prokopenko@mail.ru>
2022-04-24 00:15:51 +0300

    done burger menu

---
+++ pages/main/dist/main.dev.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// variables
+var burger = document.querySelector('.burger__wrapper-sticks');
+var sticks = document.querySelectorAll('.burger__stick');
+var header = document.querySelector('.header');
+var navBar = document.querySelector('.navbar__list');
+var navBarItems = document.querySelectorAll('.navbar__item');
+burger.addEventListener('click', burgerOpen);
+window.addEventListener('resize', checkSize);
+navBarItems.forEach(function (navBarItem) {
+  navBarItem.addEventListener('click', burgerClose);
+});
+
+if (burger.classList.contains('header-open')) {
+  var windowInnerWidth = document.documentElement.clientWidth;
+
+  if (windowInnerWidth > 768) {
+    console.log('burgerOpen');
+    burgerClose();
+  }
+}
+
+function burgerOpen() {
+  if (burger.classList.contains('header-open')) {
+    return burgerClose();
+  }
+
+  burger.classList.add('header-open');
+  navBar.classList.add('header-open');
+  header.classList.add('header-open');
+}
+
+function burgerClose() {
+  console.log("'hh");
+  var arrOfOpenElem = document.querySelectorAll('.header-open');
+  arrOfOpenElem.forEach(function (elem) {
+    elem.classList.remove('header-open');
+  });
+}
+
+function checkSize() {
+  var windowInnerWidth = document.documentElement.clientWidth;
+
+  if (windowInnerWidth > 768) {
+    burgerClose();
+  }
+}

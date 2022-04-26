4245aa29b5e8605d6298b065616dcc325fe0b346
Liza Prokopenko <elizaveta_prokopenko@mail.ru>
2022-04-26 12:37:00 +0300

    done pagination

--- pages/pets/dist/pets.dev.js
+++ pages/pets/dist/pets.dev.js
@@ -4,6 +4,14 @@ var _pets = _interopRequireDefault(require("../../assets/data/pets.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 // variables
 // for menu-burger
 var burger = document.querySelector(".burger__wrapper-sticks");
@@ -11,35 +19,53 @@ var header = document.querySelector(".header");
 var navBar = document.querySelector(".navbar__list");
 var navBarItems = document.querySelectorAll(".navbar__item"); // for gallary
 
-var newShuffledArrOfPets = new Array();
-var gallaryItems = document.querySelector(".gallary__items");
-var photos;
-var pages;
-var startNumberOfPhoto = 0;
-var startWindowInnerWidth = document.documentElement.clientWidth; // buttons
+var startNumCards = 0; // buttons
 
-var btnRigth_oneStep = document.querySelector(".btn-circle_right-oneStep");
-var btnNumber = document.querySelector(".btn-circle_number");
-var numberOfbutton = 1;
-var btnsDisabled = document.querySelectorAll(".btn-circle_disabled");
-var btnsLeft = document.querySelectorAll(".btn-circle_left");
+var btnRigth_Step = document.querySelector(".btn-circle_right-oneStep");
+var btnRight_All = document.querySelector(".btn-circle_rightAll");
 var btnsRight = document.querySelectorAll(".btn-circle_right");
+var btnLeft_All = document.querySelector(".btn-circle_leftAll");
+var btnLeft_Step = document.querySelector(".btn-circle_left-oneStep");
+var btnsLeft = document.querySelectorAll(".btn-circle_left");
+var btnNumber = document.querySelector(".btn-circle_number"); // Cards begin
+
+var shuffledArrs;
+var cards;
+var pages;
+var btnStep = 1;
+
+function initCards() {
+  var _checkWindowSizeForGa = checkWindowSizeForGallary();
+
+  var _checkWindowSizeForGa2 = _slicedToArray(_checkWindowSizeForGa, 2);
+
+  cards = _checkWindowSizeForGa2[0];
+  pages = _checkWindowSizeForGa2[1];
+  // Генерируется псевдо-массив с 48 карточками
+  shuffledArrs = createPseudorandomArr(cards, pages);
+  var shuffledArrsForRender = shuffledArrs.slice(0, cards);
+  generateCard(shuffledArrsForRender);
+}
+
+window.addEventListener("resize", checkWindowSize);
+window.addEventListener("load", initCards);
 burger.addEventListener("click", burgerOpen);
-window.addEventListener("resize", checkSize);
-window.addEventListener("load", loadPhotos);
-btnRigth_oneStep.addEventListener("click", moveGallaryOneStepRight);
 navBarItems.forEach(function (navBarItem) {
   navBarItem.addEventListener("click", burgerClose);
 });
+btnRigth_Step.addEventListener("click", moveGallaryOneStepRight);
+btnRight_All.addEventListener("click", moveGallaryAllRight);
+btnLeft_Step.addEventListener("click", moveGallaryOneStepLeft);
+btnLeft_All.addEventListener("click", moveGallaryAllLeft);
 
 if (burger.classList.contains("header-open")) {
   var windowInnerWidth = document.documentElement.clientWidth;
 
   if (windowInnerWidth > 768) {
     burgerClose();
-  } //   if (startWindowInnerWidth)
+  }
+} // burger
 
-}
 
 function burgerOpen() {
   if (burger.classList.contains("header-open")) {
@@ -58,74 +84,157 @@ function burgerClose() {
   });
 }
 
-function checkSize(event) {
+function checkWindowSize(event) {
   var windowInnerWidth = document.documentElement.clientWidth;
 
   if (windowInnerWidth > 768) {
     burgerClose();
   }
+} // gallary
 
-  if (windowInnerWidth) console.log(event);
-}
 
-function loadPhotos() {
-  // check window size and choose number of photos and pages
+function checkWindowSizeForGallary() {
   var windowInnerWidth = document.documentElement.clientWidth;
+  var cards;
+  var pages;
 
   if (1280 <= windowInnerWidth) {
-    photos = 8;
+    cards = 8;
     pages = 6;
   } else if (768 <= windowInnerWidth && windowInnerWidth <= 1280) {
-    photos = 6;
+    cards = 6;
     pages = 8;
-  } else if (768 > windowInnerWidth) {
-    photos = 3;
+  } else {
+    // 768 > windowInnerWidth
+    cards = 3;
     pages = 16;
   }
 
+  return [cards, pages];
+}
+
+function createPseudorandomArr(cards, pages) {
+  // Берет массив объектов из data.js, рандомизирует и конкатинирует в один псевдослучайный массив
+  var shuffledArrs = [];
+
   for (var page = 1; page <= pages; page++) {
-    // Создается и собирается псевдомассив из 48 элементов
     var shuffleArr = _pets["default"].map(function (i) {
       return [Math.random(), i];
     }).sort().map(function (i) {
       return i[1];
     });
 
-    newShuffledArrOfPets = newShuffledArrOfPets.concat(shuffleArr.slice(0, photos)); // console.log(`page: ${page}`)
-
-    for (var photo = 1; photo <= photos; photo++) {
-      // console.log(photo)
-      gallaryItems.innerHTML += "<div class=\"card card__pPets\">\n\t\t\t\t\t\t\t\t\t\t\t\t<div>\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t<img src=".concat(newShuffledArrOfPets[photo - 1].img, " alt=\"").concat(newShuffledArrOfPets[photo - 1].name, "\">\n\t\t\t\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t\t\t\t\t<p class=\"title card__subtitle\">").concat(newShuffledArrOfPets[photo - 1].name, "</p>\n\t\t\t\t\t\t\t\t\t\t\t\t<button class=\"btn__style-transparent\">Learn more</button>\n\t\t\t\t\t\t\t\t\t\t</div>");
-    }
+    shuffledArrs = shuffledArrs.concat(shuffleArr.slice(0, cards));
   }
 
-  return newShuffledArrOfPets, photos, pages;
+  ;
+  return shuffledArrs;
+}
+
+function generateCard(shuffledArrsForRender) {
+  var gallaryItems = document.querySelector(".gallary__items");
+  gallaryItems.innerHTML = "";
+
+  for (var card = 1; card <= shuffledArrsForRender.length; card++) {
+    gallaryItems.innerHTML += "<div class=\"card card__pPets\">\n              <div>\n                  <img src=".concat(shuffledArrsForRender[card - 1].img, " alt=\"").concat(shuffledArrsForRender[card - 1].name, "\">\n              </div>\n              <p class=\"title card__subtitle\">").concat(shuffledArrsForRender[card - 1].name, "</p>\n              <button class=\"btn__style-transparent\">Learn more</button>\n          </div>");
+  }
+}
+
+function changeNumBtn(btnStep) {
+  btnNumber.innerHTML = "<span>".concat(btnStep, "</span>");
+}
+
+function removeClasses_disabled() {
+  var btnsDisabled = document.querySelectorAll(".btn-circle_disabled");
+  btnsDisabled.forEach(function (item) {
+    return item.classList.remove("btn-circle_disabled");
+  });
 }
 
 function moveGallaryOneStepRight() {
-  // Генерация новых карточек, выбор из псевдослучайного массива
-  startNumberOfPhoto += photos;
-  var nextArr = newShuffledArrOfPets.slice(startNumberOfPhoto, startNumberOfPhoto + photos);
-  gallaryItems.innerHTML = "";
+  // Проверка на наличие класса disabled
+  if (btnRigth_Step.classList.contains("btn-circle_disabled")) {
+    return;
+  } // Убирается класс btn-circle_disabled
 
-  for (var photo = 1; photo <= photos; photo++) {
-    gallaryItems.innerHTML += "<div class=\"card card__pPets\">\n\t\t\t\t\t\t\t\t\t<div>\n\t\t\t\t\t\t\t\t\t\t\t<img src=".concat(nextArr[photo - 1].img, " alt=\"").concat(nextArr[photo - 1].name, "\">\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t\t<p class=\"title card__subtitle\">").concat(nextArr[photo - 1].name, "</p>\n\t\t\t\t\t\t\t\t\t<button class=\"btn__style-transparent\">Learn more</button>\n\t\t\t\t\t\t\t</div>");
-  }
 
-  console.log("startNumberOfPhoto", startNumberOfPhoto); // Смена номера на кнопке
+  removeClasses_disabled(); // Генерация новых карточек, выбор из псевдослучайного массива
 
-  numberOfbutton += 1;
-  btnNumber.innerHTML = "<span>".concat(numberOfbutton, "</span>"); // Убирается класс btn-circle_disabled
+  startNumCards += cards;
+  var shuffledArrsForRender = shuffledArrs.slice(startNumCards, startNumCards + cards);
+  generateCard(shuffledArrsForRender); // Смена номера на кнопке
 
-  btnsDisabled.forEach(function (item) {
-    return item.classList.remove("btn-circle_disabled");
-  }); // Проверка на конец списка
+  btnStep += 1;
+  changeNumBtn(btnStep); // Проверка на конец списка
 
-  if (startNumberOfPhoto === 42) {
+  if (startNumCards === shuffledArrs.length - cards) {
     btnsRight.forEach(function (item) {
       return item.classList.add("btn-circle_disabled");
     });
   }
+}
 
-  return startNumberOfPhoto, numberOfbutton;
+function moveGallaryOneStepLeft() {
+  // Проверка на наличие класса disabled
+  if (btnLeft_Step.classList.contains("btn-circle_disabled")) {
+    return;
+  } // Убираем класс disabled у дургих кнопок
+
+
+  removeClasses_disabled(); // Генерация новых карточек, выбор из псевдослучайного массива
+
+  startNumCards -= cards;
+  var shuffledArrsForRender = shuffledArrs.slice(startNumCards, startNumCards + cards);
+  generateCard(shuffledArrsForRender); // Кнопка с цифрой
+
+  btnStep -= 1;
+  changeNumBtn(btnStep); // Проверка на конец списка
+
+  if (startNumCards === 0) {
+    btnsLeft.forEach(function (item) {
+      return item.classList.add("btn-circle_disabled");
+    });
+  }
+}
+
+function moveGallaryAllRight() {
+  // Проверка на наличие класса disabled
+  if (btnRigth_Step.classList.contains("btn-circle_disabled")) {
+    return;
+  } // Генерация новых карточек, выбор из псевдослучайного массива
+
+
+  startNumCards = shuffledArrs.length - cards;
+  var shuffledArrsForRender = shuffledArrs.slice(startNumCards, startNumCards + cards);
+  generateCard(shuffledArrsForRender); // Кнопка с цифрой
+
+  btnStep = pages;
+  changeNumBtn(btnStep); // Убираем класс disabled у дургих кнопок
+
+  removeClasses_disabled(); // Добавляем класс disabled
+
+  btnsRight.forEach(function (item) {
+    return item.classList.add("btn-circle_disabled");
+  });
+}
+
+function moveGallaryAllLeft() {
+  // Проверка на наличие класса disabled
+  if (btnLeft_Step.classList.contains("btn-circle_disabled")) {
+    return;
+  } // Убираем класс disabled у других кнопок
+
+
+  removeClasses_disabled(); // Добавляем класс disabled
+
+  btnsLeft.forEach(function (item) {
+    return item.classList.add("btn-circle_disabled");
+  }); // Генерация новых карточек, выбор из псевдослучайного массива
+
+  startNumCards = 0;
+  var shuffledArrsForRender = shuffledArrs.slice(startNumCards, startNumCards + cards);
+  generateCard(shuffledArrsForRender); // Кнопка с цифрой
+
+  btnStep = 1;
+  changeNumBtn(btnStep);
 }
--- pages/pets/pets.js
+++ pages/pets/pets.js
@@ -8,37 +8,53 @@ const navBar = document.querySelector(".navbar__list");
 const navBarItems = document.querySelectorAll(".navbar__item");
 
 // for gallary
-let newShuffledArrOfPets = new Array();
-const gallaryItems = document.querySelector(".gallary__items");
-let photos;
-let pages;
-let startNumberOfPhoto = 0;
-let startWindowInnerWidth = document.documentElement.clientWidth;
+let startNumCards = 0;
 
 // buttons
-const btnRigth_oneStep = document.querySelector(".btn-circle_right-oneStep");
-const btnNumber = document.querySelector(".btn-circle_number");
-let numberOfbutton = 1;
-let btnsDisabled = document.querySelectorAll(".btn-circle_disabled");
-const btnsLeft = document.querySelectorAll(".btn-circle_left");
+const btnRigth_Step = document.querySelector(".btn-circle_right-oneStep");
+const btnRight_All = document.querySelector(".btn-circle_rightAll");
 const btnsRight = document.querySelectorAll(".btn-circle_right");
+const btnLeft_All = document.querySelector(".btn-circle_leftAll");
+const btnLeft_Step = document.querySelector(".btn-circle_left-oneStep");
+const btnsLeft = document.querySelectorAll(".btn-circle_left");
+const btnNumber = document.querySelector(".btn-circle_number");
 
+// Cards begin
+let shuffledArrs;
+let cards;
+let pages;
+let btnStep = 1;
+
+function initCards() {
+  [cards, pages] = checkWindowSizeForGallary();
+  // Генерируется псевдо-массив с 48 карточками
+  shuffledArrs = createPseudorandomArr(cards, pages);
+  let shuffledArrsForRender = shuffledArrs.slice(0, cards);
+  generateCard(shuffledArrsForRender);
+}
+
+
+window.addEventListener("resize", checkWindowSize);
+window.addEventListener("load", initCards);
 burger.addEventListener("click", burgerOpen);
-window.addEventListener("resize", checkSize);
-window.addEventListener("load", loadPhotos);
-btnRigth_oneStep.addEventListener("click", moveGallaryOneStepRight);
 navBarItems.forEach((navBarItem) => {
   navBarItem.addEventListener("click", burgerClose);
 });
+btnRigth_Step.addEventListener("click", moveGallaryOneStepRight);
+btnRight_All.addEventListener("click", moveGallaryAllRight);
+btnLeft_Step.addEventListener("click", moveGallaryOneStepLeft);
+btnLeft_All.addEventListener("click", moveGallaryAllLeft);
+
+
 
 if (burger.classList.contains("header-open")) {
   let windowInnerWidth = document.documentElement.clientWidth;
   if (windowInnerWidth > 768) {
     burgerClose();
   }
-  //   if (startWindowInnerWidth)
 }
 
+// burger
 function burgerOpen() {
   if (burger.classList.contains("header-open")) {
     return burgerClose();
@@ -55,83 +71,152 @@ function burgerClose() {
   });
 }
 
-function checkSize(event) {
+function checkWindowSize(event) {
   let windowInnerWidth = document.documentElement.clientWidth;
   if (windowInnerWidth > 768) {
     burgerClose();
   }
-  if (windowInnerWidth) console.log(event);
 }
 
-function loadPhotos() {
-  // check window size and choose number of photos and pages
+
+// gallary
+function checkWindowSizeForGallary() {
   let windowInnerWidth = document.documentElement.clientWidth;
+  let cards;
+  let pages;
   if (1280 <= windowInnerWidth) {
-    photos = 8;
+    cards = 8;
     pages = 6;
   } else if (768 <= windowInnerWidth && windowInnerWidth <= 1280) {
-    photos = 6;
+    cards = 6;
     pages = 8;
-  } else if (768 > windowInnerWidth) {
-    photos = 3;
+  } else { // 768 > windowInnerWidth
+    cards = 3;
     pages = 16;
   }
+  return [cards, pages];
+}
 
+function createPseudorandomArr(cards, pages) {
+  // Берет массив объектов из data.js, рандомизирует и конкатинирует в один псевдослучайный массив
+  let shuffledArrs = [];
   for (let page = 1; page <= pages; page++) {
-    // Создается и собирается псевдомассив из 48 элементов
     let shuffleArr = arrOfPets
       .map((i) => [Math.random(), i])
       .sort()
       .map((i) => i[1]);
-    newShuffledArrOfPets = newShuffledArrOfPets.concat(
-      shuffleArr.slice(0, photos)
-    );
-    // console.log(`page: ${page}`)
-    for (let photo = 1; photo <= photos; photo++) {
-      // console.log(photo)
-      gallaryItems.innerHTML += `<div class="card card__pPets">
-												<div>
-														<img src=${newShuffledArrOfPets[photo - 1].img} alt="${
-        newShuffledArrOfPets[photo - 1].name
-      }">
-												</div>
-												<p class="title card__subtitle">${newShuffledArrOfPets[photo - 1].name}</p>
-												<button class="btn__style-transparent">Learn more</button>
-										</div>`;
-    }
-  }
-  return newShuffledArrOfPets, photos, pages;
+    shuffledArrs = shuffledArrs.concat(shuffleArr.slice(0, cards));
+  };
+  return shuffledArrs;
 }
 
-function moveGallaryOneStepRight() {
-  // Генерация новых карточек, выбор из псевдослучайного массива
-  startNumberOfPhoto += photos;
-  let nextArr = newShuffledArrOfPets.slice(
-    startNumberOfPhoto,
-    startNumberOfPhoto + photos
-  );
+
+function generateCard(shuffledArrsForRender) {
+  const gallaryItems = document.querySelector(".gallary__items");
   gallaryItems.innerHTML = "";
-  for (let photo = 1; photo <= photos; photo++) {
+  for (let card = 1; card <= shuffledArrsForRender.length; card++) {
     gallaryItems.innerHTML += `<div class="card card__pPets">
-									<div>
-											<img src=${nextArr[photo - 1].img} alt="${nextArr[photo - 1].name}">
-									</div>
-									<p class="title card__subtitle">${nextArr[photo - 1].name}</p>
-									<button class="btn__style-transparent">Learn more</button>
-							</div>`;
+              <div>
+                  <img src=${shuffledArrsForRender[card - 1].img} alt="${shuffledArrsForRender[card-1].name}">
+              </div>
+              <p class="title card__subtitle">${shuffledArrsForRender[card - 1].name}</p>
+              <button class="btn__style-transparent">Learn more</button>
+          </div>`;
   }
-  console.log("startNumberOfPhoto", startNumberOfPhoto);
+}
 
-  // Смена номера на кнопке
-  numberOfbutton += 1;
-  btnNumber.innerHTML = `<span>${numberOfbutton}</span>`;
+function changeNumBtn(btnStep) {
+  btnNumber.innerHTML = `<span>${btnStep}</span>`;
+}
 
-  // Убирается класс btn-circle_disabled
+function removeClasses_disabled() {
+  let btnsDisabled = document.querySelectorAll(".btn-circle_disabled");
   btnsDisabled.forEach((item) => item.classList.remove("btn-circle_disabled"));
+}
+
+
+function moveGallaryOneStepRight() {
+  // Проверка на наличие класса disabled
+  if(btnRigth_Step.classList.contains("btn-circle_disabled")) {
+    return;
+  }
+  // Убирается класс btn-circle_disabled
+  removeClasses_disabled();
+  // Генерация новых карточек, выбор из псевдослучайного массива
+  startNumCards += cards;
+  let shuffledArrsForRender = shuffledArrs.slice(startNumCards, startNumCards+cards)
+  generateCard(shuffledArrsForRender);
+  // Смена номера на кнопке
+  btnStep +=1;
+  changeNumBtn(btnStep);
   // Проверка на конец списка
-  if (startNumberOfPhoto === 42) {
+  if (startNumCards === shuffledArrs.length - cards) {
     btnsRight.forEach((item) => item.classList.add("btn-circle_disabled"));
   }
+}
 
-  return startNumberOfPhoto, numberOfbutton;
+
+function moveGallaryOneStepLeft() {
+  // Проверка на наличие класса disabled
+  if(btnLeft_Step.classList.contains("btn-circle_disabled")) {
+    return;
+  }
+	// Убираем класс disabled у дургих кнопок
+  removeClasses_disabled()
+
+  // Генерация новых карточек, выбор из псевдослучайного массива
+  startNumCards -= cards;
+  let shuffledArrsForRender = shuffledArrs.slice(startNumCards, startNumCards+cards)
+  generateCard(shuffledArrsForRender);
+
+  // Кнопка с цифрой
+  btnStep -=1;
+  changeNumBtn(btnStep);
+  // Проверка на конец списка
+  if (startNumCards === 0) {
+    btnsLeft.forEach((item) => item.classList.add("btn-circle_disabled"));
+  }
+}
+
+
+function moveGallaryAllRight() {
+  // Проверка на наличие класса disabled
+  if (btnRigth_Step.classList.contains("btn-circle_disabled")) {
+    return;
+  }
+  // Генерация новых карточек, выбор из псевдослучайного массива
+  startNumCards = shuffledArrs.length - cards;
+  let shuffledArrsForRender = shuffledArrs.slice(startNumCards, startNumCards+cards)
+  generateCard(shuffledArrsForRender);
+
+  // Кнопка с цифрой
+  btnStep = pages;
+  changeNumBtn(btnStep);
+
+	// Убираем класс disabled у дургих кнопок
+  removeClasses_disabled()
+  // Добавляем класс disabled
+  btnsRight.forEach((item) => item.classList.add("btn-circle_disabled"));
+}
+
+function moveGallaryAllLeft() {
+    // Проверка на наличие класса disabled
+    if (btnLeft_Step.classList.contains("btn-circle_disabled")) {
+      return;
+    }
+
+    // Убираем класс disabled у других кнопок
+    removeClasses_disabled();
+
+    // Добавляем класс disabled
+    btnsLeft.forEach((item) => item.classList.add("btn-circle_disabled"));
+
+    // Генерация новых карточек, выбор из псевдослучайного массива
+    startNumCards = 0;
+    let shuffledArrsForRender = shuffledArrs.slice(startNumCards, startNumCards+cards)
+    generateCard(shuffledArrsForRender);
+  
+    // Кнопка с цифрой
+    btnStep = 1;
+    changeNumBtn(btnStep);
 }
